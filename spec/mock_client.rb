71c2886d39e57c715dac00d46fd8f74b0fa7d86b
Daniel Pepper <pepper.daniel@gmail.com>
2022-10-06 08:48:50 -0700

    tests

--- spec/mock_client.rb
+++ spec/mock_client.rb
@@ -3,7 +3,7 @@ describe "mock_twirp_client" do
 
   let(:client_class) { GoodbyeClient }
   let(:responses) { {} }
-  let(:request) { GoodbyeRequest.new }
+  let(:request) { GoodbyeRequest.new(name: "Alfred") }
   let(:res) { client.bye(request) }
 
   it { is_expected.to be_a(Twirp::Client) }
@@ -20,15 +20,21 @@ describe "mock_twirp_client" do
   end
 
   context "when responses arg is specified" do
-    let(:responses) { { bye: { message: "adios" } } }
+    let(:responses) { { bye: request } }
 
-    it { expect(res).to be_a_twirp_response(message: "adios") }
+    it { expect(res).to be_a_twirp_response(name: /Alf/) }
 
     it "raises when unstubbed methods are called" do
       expect {
         client.goodbye(request)
       }.to raise_error(RSpec::Mocks::MockExpectationError)
     end
+  end
+
+  context "when responses attributes are specified" do
+    let(:responses) { { bye: { message: "adios" } } }
+
+    it { expect(res).to be_a_twirp_response(message: "adios") }
   end
 
   context "bogus methods are stubbed" do
